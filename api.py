ad5644b8837005fdc70a7146dc6e834d38a826c2
Masoodaaafreen <162811384+Masoodaaafreen@users.noreply.github.com>
2025-07-05 20:00:07 +0530

    api.py

--- api.py
+++ api.py
@@ -1,13 +1,9 @@
 import requests
-
-API_KEY = 'your_api_key_here'  # Replace with your OpenWeatherMap API key
+API_KEY = "Put your API KEY here"
 city = input("Enter city name: ")
-
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-
 response = requests.get(url)
 data = response.json()
-
 if response.status_code == 200:
     print(f"City: {data['name']}")
     print(f"Weather: {data['weather'][0]['description']}")
